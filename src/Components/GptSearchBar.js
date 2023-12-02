5013f10ebca9f15494a1c0895fff99d6a58d1547
akshay77 <ajakshayjain11@gmail.com>
2023-12-02 14:52:46 +0530

    cleared all slices

--- src/Components/GptSearchBar.js
+++ src/Components/GptSearchBar.js
@@ -73,26 +73,30 @@ const GptSearchBar = () => {
 
 
   return (
-    <div className="pt-[35%] md:pt-[10%] flex justify-center">
-      <form
-        className=" w-full  md:w-1/2 bg-black grid grid-cols-12"
-        onSubmit={(e) => e.preventDefault()}
-      >
-        <input
-          ref={searchText}
-          type="text"
-          className=" p-4 m-4 col-span-9"
-          placeholder={lang[languageKey].GptplaceHolder}
-        />
-        <button
-          onClick={Handlesearchbutton}
-          className="py-2 px-4  m-4 bg-red-700 text-white rounded-lg col-span-3"
+    <>
+      
+
+      <div className="pt-[35%] md:pt-[10%] flex justify-center">
+        <form
+          className=" w-full  md:w-1/2 bg-black grid grid-cols-12"
+          onSubmit={(e) => e.preventDefault()}
         >
-          {lang[languageKey].search}
-        </button>
-        {Isloading ? <Shimmereffet /> : null}
-      </form>
-    </div>
+          <input
+            ref={searchText}
+            type="text"
+            className=" p-4 m-4 col-span-9"
+            placeholder={lang[languageKey].GptplaceHolder}
+          />
+          <button
+            onClick={Handlesearchbutton}
+            className="py-2 px-4  m-4 bg-red-700 text-white rounded-lg col-span-3"
+          >
+            {lang[languageKey].search}
+          </button>
+          {Isloading ? <Shimmereffet /> : null}
+        </form>
+      </div>
+    </>
   );
 };
 
